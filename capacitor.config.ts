72a1f6b849bd3aa20904f4dfaf0410443f936e27
SamirSierra <samir.sierramaza@unicolombo.edu.co>
2024-11-06 04:11:32 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ParcialFinal',
+  webDir: 'www'
+};
+
+export default config;
